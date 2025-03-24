27e0a8dceefc58252529435903e4eb86bfc06aea
Jason Smith <jalmasmith@gmail.com>
2025-03-24 11:38:02 -0600

    Initial Commit

---
+++ CricutShapesTests/CricutShapesTests.swift
@@ -0,0 +1,16 @@
+//
+//  CricutShapesTests.swift
+//  CricutShapesTests
+//
+//  Created by Jason Smith on 3/24/25.
+//
+
+import Testing
+
+struct CricutShapesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
